ef85c5cee0e429eef823be18398131dc8b073530
sergey <bersercwow@gmail.com>
2024-04-19 13:59:06 +0700

    The class must support methods for comparing vacancies with each other by salary and validate the data that initializes its attributes.

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,12 +1,16 @@
 class Vacancy:
     name: str
+    link: str
     to_salary: int
     from_salary: int
     requirements: str
     responsibility: str
 
-    def __init__(self, name, url, to_salary, from_salary, requirements, responsibility):
+    def __init__(self, name: str, link: str, to_salary: int,
+                 from_salary: int, requirements: str, responsibility: str
+                 ):
         self.name = name
+        self.link = link
         self.to_salary = to_salary
         self.from_salary = from_salary
         self.requirements = requirements
@@ -22,3 +26,33 @@ class Vacancy:
                 f"Оплата: от {self.from_salary} до {self.to_salary}\n"
                 f"Требования: {self.requirements}\n"
                 f"Должностные обязанности: {self.responsibility}")
+
+    def __lt__(self, other) -> bool:
+        """
+        Метод сравнивает зарплаты вакансий.
+        :param other: null
+        :return: True
+        """
+        if self.to_salary is None or other.to_salary is None:
+            return False
+        return self.to_salary < other.to_salary
+
+    def __gt__(self, other) -> bool:
+        """
+        Метод сравнивает зарплаты вакансий.
+        :param other: null
+        :return: True
+        """
+        if self.to_salary is None or other.to_salary is None:
+            return False
+        return self.to_salary > other.to_salary
+
+    def __eq__(self, other) -> bool:
+        """
+        Метод сравнивает зарплаты вакансий.
+        :param other: null
+        :return: True
+        """
+        if self.to_salary is None or other.to_salary is None:
+            return False
+        return self.to_salary == other.to_salary
